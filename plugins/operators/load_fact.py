eb31efd65c36c121f638019d0f0655dd92042b73
ljia-ch <linjia.iphy@gmail.com>
2021-09-12 10:37:56 -0500

    add load fact operator

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -2,7 +2,18 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+from helpers import sql_queries
+
 class LoadFactOperator(BaseOperator):
+    """
+    Load data from staging tables to Fact tables
+    
+    [Parameters List]
+    redshift_conn_id:    AWS redshift connection id 
+    target_table:        Fact table name
+    target_columns:      Fact table column names 
+    query:               Query names to use in sql_queries
+    """
 
     ui_color = '#F98866'
 
