3478effd9ae628bb65a0cdaf5ce2511cfd4e79f3
ljia-ch <linjia.iphy@gmail.com>
2021-10-04 04:30:52 +0800

    add code

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -118,3 +118,24 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> create_tables
+
+create_tables >> stage_events_to_redshift
+create_tables >> stage_events_to_redshift
+
+stage_events_to_redshift >> load_songplays_table
+
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+
+load_user_dimension_table >> run_quality_checks
+load_song_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+
+run_quality_checks >> end_operator 
+
+
